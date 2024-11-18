b788478bdb99c0b9b727d2709b10126fffcab5e1
DARK14541 <fa5835018@gmail.com>
2024-11-18 17:04:18 +0530

    Update oneword.py

--- SHUKLA/plugins/tools/oneword.py
+++ SHUKLA/plugins/tools/oneword.py
@@ -9,7 +9,7 @@ from ... import *
 from SHUKLA.modules.SHASHANK.data import OneWord
 
 
-FC = 3
+FC = 2
 
 
 @app.on_message(cdz(["randi"])  & (filters.me | filters.user(SUDO_USER)))
@@ -21,7 +21,7 @@ async def alt_lol(xspam: Client, message: Message):
     try:
         for word in OneWord:
             await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
-            await asyncio.sleep(1)
+            await asyncio.sleep(0)
     except FloodWait:
         print("Flood !!")
         pass
@@ -38,21 +38,6 @@ async def alt_mkc(xspam: Client, message: Message):
         for word in OneWord:
             await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
             await asyncio.sleep(000.1)
-    except FloodWait:
-        print("Flood !!")
-        pass
- 
-
-   @app.on_message(cdz(["madarchod"])  & (filters.me | filters.user(SUDO_USER)))
-async def alt_mkc(xspam: Client, message: Message):    
-    chat_id = message.chat.id
-    RUSH = None
-    if message.reply_to_message:
-        RUSH = message.reply_to_message.id
-    try:
-        for word in OneWord:
-            await xspam.send_message(chat_id, word, reply_to_message_id=RUSH)
-            await asyncio.sleep(0.0)
     except FloodWait:
         print("Flood !!")
         pass
